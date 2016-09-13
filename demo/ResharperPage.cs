2efb4787df520dac872d47166144b9442529773a
Yurii Myslyvets <yurii.myslyvets@gmail.com>
2016-09-13 22:40:54 +0300

    Demo project is added

---
+++ demo/ResharperPage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Find.By.Demo
+{
+    //https://resharper-plugins.jetbrains.com/packages/Find.By/
+    public class ResharperPage
+    {   
+        [FindsBy(How = How.XPath, Using = "//img[@class='logo']")]
+        public IWebElement Icon { get; set; }
+
+        [FindsBy(How = How.LinkText, Using = "Download")]
+        public IWebElement Download { get; set; }
+
+        [FindsBy(How = How.TagName, Using = "a")]
+        public IWebElement AllLinks { get; set; }
+
+        [FindsBy(How = How.TagName, Using = "div")]
+        public IWebElement AllDivs { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "ol")]
+        public IWebElement Spans { get; set; }
+    }
+}

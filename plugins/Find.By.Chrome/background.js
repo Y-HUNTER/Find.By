6fd969be9af0550f720749f16208154ab98f2de8
Yurii Myslyvets <yurii.myslyvets@gmail.com>
2016-11-07 22:40:11 +0200

    Removed number of elements

--- plugins/Find.By.Chrome/background.js
+++ plugins/Find.By.Chrome/background.js
@@ -2,35 +2,23 @@ var isRunning = false;
 
 function updateIcon() {
   isRunning = !isRunning;
-  chrome.browserAction.setIcon({ path: isRunning ? "./img/iconOn.png" : "./img/iconOff.png" });
-  if (!isRunning) turnOffAllTabs();
-}
-
-function turnOffAllTabs() {
-  chrome.tabs.query({ currentWindow: true }, function (tabs) {
-    for (var i = 0; i < tabs.length; i++) {
-      chrome.tabs.sendMessage(tabs[i].id, { locator: null, isRunning: isRunning });
-    }
-  });
-}
-
-function showNumberOfLocators(message) {
-  chrome.browserAction.setBadgeText({ text: message.number });
+  chrome.browserAction.setIcon({path: isRunning? "./img/iconOn.png" : "./img/iconOff.png"});
 }
 
 function startingListener() {
-  setInterval(function () {
-    if (!isRunning) return;
+  setInterval(function(){
+    if(!isRunning) return;
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://localhost:32081/", false);
     xhr.send();
     var result = xhr.responseText;
-    if (!isRunning) turnOffAllTabs();
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { locator: result, isRunning: isRunning }, showNumberOfLocators);
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+      for (var i = 0; i < tabs.length; i++) {
+        chrome.tabs.sendMessage(tabs[i].id, {locator: result, isRunning: isRunning});
+      }      
     });
   }, 100);
 }
-chrome.browserAction.setBadgeBackgroundColor({ color: [0, 200, 0, 120] });
+
 chrome.browserAction.onClicked.addListener(updateIcon);
 startingListener();
